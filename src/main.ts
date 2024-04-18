da3a4962ebb663fb76f28d253a7c408f93b06d01
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-04-18 15:45:45 -0600

    Se configura archivo de variables de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,7 @@ async function bootstrap() {
     }) 
   );
 
-  await app.listen(3000);
+  await app.listen(envs.port);
+  console.log(`Aplicación de productos corriendo en el puerto ${envs.port}`);
 }
 bootstrap();
