79e5df36d89f6827841c90aca8616f4cde8a6d3e
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-04-19 12:13:55 -0600

    Se inicia la migración a microservicio

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
 
   const logger = new Logger('ProductosMs-main');
 
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envs.port
+      }
+    },
+  );
 
   app.useGlobalPipes(  
     new ValidationPipe({ 
@@ -16,7 +25,7 @@ async function bootstrap() {
     }) 
   );
 
-  await app.listen(envs.port);
-  logger.log(`Aplicación de productos corriendo en el puerto ${envs.port}`);
+  await app.listen();
+  logger.log(`Microservicio de productos corriendo en el puerto ${envs.port}`);
 }
 bootstrap();
