f8c5e9158f790625411571a413527ef60cd32c92
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2024-04-29 10:45:24 -0600

    Se inicia configuración de NATS

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,15 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
 
   const logger = new Logger('ProductosMs-main');
+  
+  console.log(envs.natsServers);
 
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
-      transport: Transport.TCP,
+      transport: Transport.NATS,
       options: {
-        port: envs.port
+        servers: envs.natsServers,
       }
     },
   );
